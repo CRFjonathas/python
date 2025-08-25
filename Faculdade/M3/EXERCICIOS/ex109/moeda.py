6238a20443e6e06698afa8419ed7d9e7961f42e9
Jonathas Nicacio Silva <jonathas.github@gmail.com>
2025-08-25 17:57:15 -0300

    feat: adiciona suporte para formatação de moeda nas funções de manipulação de preço

--- Faculdade/M3/EXERCICIOS/ex109/moeda.py
+++ Faculdade/M3/EXERCICIOS/ex109/moeda.py
@@ -1,16 +1,18 @@
-def metade(preço):
-    return preço / 2
+def metade(preço, formato=False):
+    resultado = preço / 2
+    return resultado if not formato else moeda(resultado)
 
-def dobro(preço):
-    return preço * 2
+def dobro(preço, formato=False):
+    resultado = preço * 2
+    return resultado if not formato else moeda(resultado)
 
-def aumentar(preço, porcentagem):
-    aumento = (porcentagem / 100) * preço
-    return preço + aumento
+def aumentar(preço, porcentagem, formato=False):
+    aumento = preço + (preço * porcentagem /100)
+    return aumento if not formato else moeda(aumento)
 
-def diminuir(preço, porcentagem):
-    desconto = (porcentagem / 100) * preço
-    return preço - desconto
+def diminuir(preço, porcentagem, formato=False):
+    desconto = preço - (preço * porcentagem /100)
+    return desconto if not formato else moeda(desconto)
 
 def moeda(preço=0, moeda='R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
