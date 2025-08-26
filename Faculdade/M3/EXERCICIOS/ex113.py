be43ff9e15bd8d3e6ca77b8279b393ffc9abe138
Jonathas Nicacio Silva <jonathas.github@gmail.com>
2025-08-26 14:58:45 +0000

    feat: adiciona a solução do exercicio 113

---
+++ Faculdade/M3/EXERCICIOS/ex113.py
@@ -0,0 +1,29 @@
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um numero inteirio válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuario preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+        
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um numero inteirio válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuario preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+num = leiaInt('Digite um valor: ')
+num2 = leiaFloat('Digite um valor: ')
+print(f'O valor digitado foi {num}')
