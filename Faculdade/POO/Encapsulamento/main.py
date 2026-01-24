be6e1538eceb8a1efa9149f32c945e9bdbd780c2
Jonathas Nicacio <jonathas.github@gmail.com>
2026-01-24 23:43:25 +0000

    feat: cria um programa usando encapsulamento

---
+++ Faculdade/POO/Encapsulamento/main.py
@@ -0,0 +1,13 @@
+from classes.conta import Conta
+
+c1 = Conta(1, 1000)
+c2 = Conta(2, 50)
+c1.sacar(200)
+
+c1.saldo = -1500
+
+print(f"Até agora temos {Conta.get_total_contas()} cconta(s) criada(s)")
+print(f"Muito Obrigado por ser cliente do {Conta.nome_banco()}")
+
+c1.gerar_saldo()
+print(f"Conta 2 com R$ {c2.saldo}")
