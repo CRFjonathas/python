671c19f5aa18e8bfa283a05178ed6b7338aa9185
Jonathas Nicacio <jonathas.github@gmail.com>
2026-03-22 16:48:54 +0000

    fix: corrige o diretorio de destino do arquivo gerado

--- Faculdade/RAD(Rapid Applications Development)/Manipulação-de-Dados-em-Arquivos/Funções-de-manipulação-de-arquivos/Operações-Basicas/OperacoesBasicas.py
+++ Faculdade/RAD(Rapid Applications Development)/Manipulação-de-Dados-em-Arquivos/Funções-de-manipulação-de-arquivos/Operações-Basicas/OperacoesBasicas.py
@@ -1,6 +1,6 @@
 import os
 
-arquivo1 = open("teste.txt", "w", encoding="utf-8")
+arquivo1 = open("/workspaces/python/Faculdade/RAD(Rapid Applications Development)/Manipulação-de-Dados-em-Arquivos/Funções-de-manipulação-de-arquivos/Operações-Basicas/teste.txt", "w", encoding="utf-8")
 print(os.path.abspath(arquivo1.name))
 
 arquivo1.write("Olà, mundo!")
