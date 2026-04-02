a77c62613e54da1b24c5782beb4079f890c0bd62
CRFjonathas <jonaths.github@gmail.com>
2026-04-02 16:17:56 -0300

    refactor(Livro): ajusta a lógica de paginação e melhora a exibição de mensagens

--- Python-Orientado-a-Objeto/desafios/Desafio 19/classes/Livro.py
+++ Python-Orientado-a-Objeto/desafios/Desafio 19/classes/Livro.py
@@ -1,27 +1,28 @@
 from rich import print
-from time import sleep
+import time
 
 class Livro:
     def __init__(self, titulo, paginas):
         self.titulo = titulo
-        self.paginas = paginas
+        self.total_paginas = paginas
         self.pagina_atual = 1
         
-        print(f"[blue]Você acabou de abrir o livro '[red]{self.titulo}[/]' que tem [green]{self.paginas} paginas[/] no total. Você agora está na [yellow]página 1[/][/]")
+        print(f":open_book: [blue]Você acabou de abrir o livro '[red]{self.titulo}[/]' que tem [green]{self.total_paginas} paginas[/] no total. Você agora está na [yellow]página {self.pagina_atual}[/][/]")
 
-    def avancar_pagina(self, pag):
-        
-        for i in range(pag):
-            self.pagina_atual +=1
-            print(f"Pág{self.pagina_atual} ->", end=" ")
-            sleep(0.3)
-            
-            if self.pagina_atual >= self.paginas:
-                pag = i + 1
-                break
+    def avancar_pagina(self, qtd = 1):
+        count = 0
+        for pg in range(0, qtd, 1):
+            if not self.fim_do_livro():
+                self.pagina_atual += 1
+                print(f"Pág{self.pagina_atual} :arrow_forward:", end=' ')
+                count += 1
+                time.sleep(0.2)
+                
+        print(f"[blue]Você avançou {count} páginas e agora está na [green]página {self.pagina_atual}[/][/]")
 
-        print(f"[blue]Você avançou {pag} páginas e agora está na [green]página {self.pagina_atual}[/][/]")
+        if self.fim_do_livro():
+            print(f":closed_book: [red]Você chegou ao final do livro '{self.titulo}'[/]")
 
-        if self.pagina_atual == self.paginas:
-            print(f"[red]Você chegou ao final do livro '{self.titulo}'[/]")
+    def fim_do_livro(self) -> bool:
+        return True if self.pagina_atual == self.total_paginas else False
                
--- Python-Orientado-a-Objeto/desafios/Desafio 19/desafio19.py
+++ Python-Orientado-a-Objeto/desafios/Desafio 19/desafio19.py
@@ -4,4 +4,4 @@ l1 = Livro("Percy Jackson - Mar de Monstros", 20)
 
 l1.avancar_pagina(5)
 l1.avancar_pagina(10)
-l1.avancar_pagina(100)
+l1.avancar_pagina(50)
