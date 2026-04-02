2d274caaf1987c98cb8e0af50afb1dba22a1737f
CRFjonathas <jonaths.github@gmail.com>
2026-04-02 14:42:31 -0300

    refactor(Churrasco): simplifica cálculos e melhora a legibilidade do método analisar

--- Python-Orientado-a-Objeto/desafios/Desafio 18/classes/Churrasco.py
+++ Python-Orientado-a-Objeto/desafios/Desafio 18/classes/Churrasco.py
@@ -2,15 +2,31 @@ from rich import print
 from rich.panel import Panel
 
 class Churrasco:
+    # Atributos de Classe
+    consumo_padrao:float = 0.400
+    preco_kg:float = 82.40
+
     def __init__(self, titulo, quant):
         self.titulo = titulo
-        self.quant = quant
+        self.participantes = quant
+
+    def qtd_carne(self) -> float:
+       return self.participantes * Churrasco.consumo_padrao
+    
+    def custo_total(self) -> float:
+        return self.qtd_carne() * Churrasco.preco_kg
+    
+    def custo_individual(self) -> float:
+        return self.custo_total() / self.participantes
 
     def analisar(self):
-        consumo = 0.4 * self.quant
-        custo = consumo * 82.40
-        rateio = custo // self.quant
 
-        painel = Panel(f"Analisando [green]{self.titulo}[/] com [blue]{self.quant} convidados[/] \nCada participante comerá 0.4KG e cada Kg custa R$82.40 \nRecomendo [blue]comprar {consumo:.3f}Kg[/] de carne \nO custo total será de [green]R${custo:.2f}[/]\nCada pessoa pagará [yellow]R${rateio:.2f}[/] para participar.", title=self.titulo, width=80)
+        conteudo = f"Analisando [green]{self.titulo}[/] com [blue]{self.participantes} convidados[/]"
+        conteudo += f"\nCada participante comerá {Churrasco.consumo_padrao:.3f}KG e cada Kg custa R${Churrasco.preco_kg:.2f}"
+        conteudo += f"\nRecomendo [blue]comprar {self.qtd_carne():.3f}Kg[/] de carne"
+        conteudo += f"\nO custo total será de [green]R${self.custo_total():.2f}[/]"
+        conteudo += f"\nCada pessoa pagará [yellow]R${self.custo_individual():.2f}[/] para participar."
+
+        painel = Panel(conteudo, title=self.titulo, width=80)
 
         print(painel)
