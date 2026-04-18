7cea752e923de4ca7fbe7141467e64ebf0daa5c2
CRFjonathas <jonaths.github@gmail.com>
2026-04-18 18:48:06 -0300

    feat: cria programa utilizando utilizando fundamentos de abstração

---
+++ Python-Orientado-a-Objeto/exercicios/ex007/classes.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+class Pessoa(ABC):
+    def __init__(self, nome = "", idade = 0):
+        self.nome = nome
+        self.idade = idade
+
+    def fazer_aniversario(self):
+        self.idade += 1
+
+    @abstractmethod
+    def estudar():
+        pass
+
+class Aluno(Pessoa):
+    def __init__(self, nome, idade, curso, turma):
+        super().__init__(nome, idade)
+        self.curso = curso
+        self.turma = turma
+
+    def fazer_matricula(self):
+        print(f"O aluno {self.nome} acabou de fazer a matricula")
+
+    def estudar(self):
+        print(f"O aluno {self.nome} esta estdando {self.curso} na turma {self.turma}")
+
+
+class Professor(Pessoa):
+    def __init__(self, nome, idade, especialidade, nivel):
+        super().__init__(nome, idade)
+        self.especialidade = especialidade
+        self.nivel = nivel
+
+    def dar_aula(self):
+        print(f"O professor {self.nome} iniciou a aula")
+
+    def estudar(self):
+        print(f"O professor {self.nome} é especialista em {self.especialidade} no {self.nivel}")
+
+    
+class Funcionario(Pessoa):
+    def __init__(self, nome, idade, cargo, setor):
+        super().__init__(nome, idade)
+        self.cargo = cargo
+        self.setor = setor
+
+    def bater_ponto(self):
+        print(f"O funcionario {self.nome} acabou de bater ponto")
+
+    def estudar(self):
+        print(f"O funcionario {self.nome} se especializa para a area de {self.setor}")
+
